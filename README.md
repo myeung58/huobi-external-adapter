7b620e4df326e8ee32145de8210b1cd89df5c0b3
myeung58 <5367108+myeung58@users.noreply.github.com>
2021-03-12 12:40:04 -0500

    v0.1.1

---
+++ README.md
@@ -0,0 +1,45 @@
+# Chainlink NodeJS External Adapter - Huobi API
+
+## Install Locally
+
+Install dependencies:
+
+```bash
+yarn
+```
+
+### Test
+
+Run the local tests:
+
+```bash
+yarn test
+```
+
+Natively run the application (defaults to port 8080):
+
+### Run
+
+```bash
+yarn start
+```
+
+## Call the external adapter/API server
+
+```bash
+curl -X POST -H "content-type:application/json" "http://localhost:8080/" --data '{ "id": 0, "data": { "from": "ETH", "to": "USD" } }'
+```
+
+## Docker
+
+If you wish to use Docker to run the adapter, you can build the image by running the following command:
+
+```bash
+docker build . -t external-adapter
+```
+
+Then run it with:
+
+```bash
+docker run -p 8080:8080 -it external-adapter:latest
+```
---
+++ index.js
@@ -0,0 +1,92 @@
+const { Requester, Validator } = require('@chainlink/external-adapter')
+
+// Define custom error scenarios for the API.
+// Return true for the adapter to retry.
+const customError = (data) => {
+  if (data.Response === 'Error') return true
+  return false
+}
+
+// Define custom parameters to be used by the adapter.
+// Extra parameters can be stated in the extra object,
+// with a Boolean value indicating whether or not they
+// should be required.
+const customParams = {
+  base: ['base', 'from', 'coin'],
+  quote: ['quote', 'to', 'market'],
+  endpoint: false
+}
+
+const createRequest = (input, callback) => {
+  // The Validator helps you validate the Chainlink request data
+  console.log("input", input)
+  const validator = new Validator(callback, input, customParams)
+  const jobRunID = validator.validated.id
+  console.log('Validated data', validator.validated.data)
+
+  const base = validator.validated.data.base.toLowerCase()
+  let quote = validator.validated.data.quote.toLowerCase()
+
+  if (quote == "usd") {
+    console.log(`Modifying quote from USD to USDT`)
+    quote = "usdt"
+  }
+
+  const url = `https://api.huobi.pro/market/trade?symbol=${base}${quote}`
+
+  const path = ['tick','data','0','price']
+  const params = {}
+
+  // This is where you would add method and headers
+  // you can add method like GET or POST and add it to the config
+  // The default is GET requests
+  // method = 'get' 
+  // headers = 'headers.....'
+  const config = {
+    url,
+    params
+  }
+
+  // The Requester allows API calls be retry in case of timeout
+  // or connection failure
+  Requester.request(config, customError)
+    .then(response => {
+      response.data.result = Requester.validateResultNumber(response.data, path)
+      callback(response.status, Requester.success(jobRunID, response))
+    })
+    .catch(error => {
+      callback(500, Requester.errored(jobRunID, error))
+    })
+}
+
+// This is a wrapper to allow the function to work with
+// GCP Functions
+exports.gcpservice = (req, res) => {
+  createRequest(req.body, (statusCode, data) => {
+    res.status(statusCode).send(data)
+  })
+}
+
+// This is a wrapper to allow the function to work with
+// AWS Lambda
+exports.handler = (event, context, callback) => {
+  createRequest(event, (statusCode, data) => {
+    callback(null, data)
+  })
+}
+
+// This is a wrapper to allow the function to work with
+// newer AWS Lambda implementations
+exports.handlerv2 = (event, context, callback) => {
+  createRequest(JSON.parse(event.body), (statusCode, data) => {
+    callback(null, {
+      statusCode: statusCode,
+      body: JSON.stringify(data),
+      isBase64Encoded: false
+    })
+  })
+}
+
+// This allows the function to be exported for testing
+// or for running in express
+module.exports.createRequest = createRequest
